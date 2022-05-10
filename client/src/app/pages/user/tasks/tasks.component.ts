6e5b422481f3a2a658e3f599ffc72d0411b2364c
Arsen <arsen.danylenko@gmail.com>
2022-05-10 18:39:27 +0300

    added new inputs

--- client/src/app/pages/user/tasks/tasks.component.ts
+++ client/src/app/pages/user/tasks/tasks.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import { TaskService } from '@services';
+
 @Component({
 	selector: 'tasks',
 	templateUrl: './tasks.component.html',
 	styleUrls: ['./tasks.component.scss']
 })
 export class TasksComponent{
-	constructor() {}
+	public task: {
+		name: any;
+		description: any;
+	};
+	constructor(
+		public ts: TaskService) {}
 }
