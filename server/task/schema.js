e08533f4a2c81666b29b4c617057b5fc5046f412
Arsen <arsen.danylenko@gmail.com>
2022-05-11 15:05:56 +0300

    I creates date and time inputs. Also each task now is a clickable button which opens the modal for editing

--- server/task/schema.js
+++ server/task/schema.js
@@ -2,6 +2,8 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema({
 	name: String,
 	description: String,
+	time: Number,
+	date: Number,
 	author: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 	moderators: [{type: mongoose.Schema.Types.ObjectId, sparse: true, ref: 'User'}],
 	url: {type: String, unique: true, sparse: true, trim: true}
@@ -12,6 +14,8 @@ Schema.methods.create = function(obj, user, sd) {
 	this.moderators = [user._id];
 	this.name = obj.name;
 	this.description = obj.description;
+	this.time = obj.time;
+	this.date = obj.date;
 }
 
 module.exports = mongoose.model('Task', Schema);
