86c7704041cdc787b6a326c87bcf5eca75b35f4a
Arsen <arsen.danylenko@gmail.com>
2022-06-01 12:46:11 +0300

    fixed the bug with saving unexistent tasks and added new design features to the modal

--- client/src/app/modals/task/task.component.ts
+++ client/src/app/modals/task/task.component.ts
@@ -3,12 +3,14 @@
 import { Component } from '@angular/core';
 import { TaskService } from '@services'
 import { ModalService } from 'wacom';
+
 @Component({
 	selector: 'task',
 	templateUrl: './task.component.html',
 	styleUrls: ['./task.component.scss']
 })
 export class TaskComponent{
+
 	public task: any = {};
 	
 	constructor(
