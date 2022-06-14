581ccfacfaa7d10335e452e2388422286428a431
Arsen <arsen.danylenko@gmail.com>
2022-06-14 15:51:08 +0300

    added capasitor for the android platform

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LocalNotifications } from '@capacitor/local-notifications';
 
 @Component({
 	selector: 'app-root',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
 	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-	title = 'To Do App';
+	title = 'My Planner';
 }
+
+
+LocalNotifications.schedule({
+	notifications: [
+	{
+		title: 'Hello',
+		body: "Widgets are 10% off. Act fast!",
+		id: 1,
+		schedule: { at: new Date(Date.now() + 1000 * 5) },
+		sound: null,
+		attachments: null,
+		actionTypeId: "",
+		extra: null
+	}
+	]
+});
